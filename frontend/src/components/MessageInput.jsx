b834e74da999ce371c22ac9ff829df9a6190e374
Avinash01 <avinash.m1302@gmail.com>
2024-08-02 23:29:59 +0530

    my first commit

---
+++ frontend/src/components/MessageInput.jsx
@@ -0,0 +1,76 @@
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import { useState } from "react";
+import { IoSendSharp } from "react-icons/io5";
+import useShowToast from "../hooks/useShowToast";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { conversationsAtom, selectedConversationAtom } from "../atoms/messagesAtom";
+
+const MessageInput = ({ setMessages }) => {
+  const [messageText, setMessageText] = useState("");
+  const showToast = useShowToast();
+  const selectedConversation = useRecoilValue(selectedConversationAtom);
+  const setConversations = useSetRecoilState(conversationsAtom);
+
+
+
+
+  const handleSendMessage = async (e) => {
+    e.preventDefault();
+    if (!messageText) return;
+    try {
+      const res = await fetch("/api/messages", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          message: messageText,
+          recipientId: selectedConversation.userId,
+        }),
+      });
+      const data = await res.json();
+      if (data.error) {
+        showToast("Error", data.error, "error");
+        return;
+      }
+      setMessages((messages) => [...messages, data]);
+
+      setConversations((prevConvs) => {
+				const updatedConversations = prevConvs.map((conversation) => {
+					if (conversation._id === selectedConversation._id) {
+						return {
+							...conversation,
+							lastMessage: {
+								text: messageText,
+								sender: data.sender,
+							},
+						};
+					}
+					return conversation;
+				});
+				return updatedConversations;
+			});
+      setMessageText("");
+      
+    } catch (error) {
+      showToast("Error", error.message, "error");
+    }
+  };
+  return (
+    <form onSubmit={handleSendMessage}>
+      <InputGroup>
+        <Input
+          w={"full"}
+          placeholder="Type a message"
+          value={messageText}
+          onChange={(e) => setMessageText(e.target.value)}
+        />
+        <InputRightElement cursor={"pointer"} onClick={handleSendMessage}>
+          <IoSendSharp />
+        </InputRightElement>
+      </InputGroup>
+    </form>
+  );
+};
+
+export default MessageInput;
